a801596db2103c13e80d4c2c24e02d398bc1f46f
BFO1715 <bethan@johnwhiteandson.com>
2022-12-08 13:56:45 +0000

    Event listener added for submit button, attempting to add alert

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,38 @@
  */
 
 function calc() {
-    var input1 = parseInt(document.getElementById("inweight").value);
-    var input2 = parseInt(document.getElementById("outweight").value);
-    // to make sure that they are numbers
+    let input1 = parseInt(document.getElementById("inweight").value);
+    let input2 = parseInt(document.getElementById("outweight").value);
     if (!input1) { input1 = 0; }
     if (!input2) { input2 = 0; }
-    var total = document.getElementById("netweight");
+    let total = document.getElementById("netweight");
     total.value = input1 - input2;
+
+    target = parseInt(document.getElementById("targetweight"));
+    if (target === total) {
+        alert(`Target Weight met`);
+    } else {
+        alert(`Target Weight not met - note vehicle registration`);
+    }  
 }
 
 /**
  * Add event listener to the submit button
  */
+
+ let counterDisplayElem = document.querySelector('.counter-display');
+ let counterPlusElem = document.querySelector('.counter-plus');
+ 
+ let count = 0;
+ 
+ updateDisplay();
+ 
+ counterPlusElem.addEventListener("click",()=>{
+     count++;
+     updateDisplay();
+ }) ;
+ 
+ function updateDisplay(){
+     counterDisplayElem.innerHTML = count;
+ };
 
